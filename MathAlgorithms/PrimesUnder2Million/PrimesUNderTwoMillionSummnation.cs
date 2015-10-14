7b4a1f127e6983f03582df93825e8a6ff9f2afb3
Spencer Nussrallah <spencer.nussrallah@gmail.com>
2015-10-14 10:58:15 -0500

    One of many solutions to Problem 10 in Project Euler look at your own discretion

---
+++ MathAlgorithms/PrimesUnder2Million/PrimesUNderTwoMillionSummnation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimesUnder2Million
+{
+    class PrimesUNderTwoMillionSummnation
+    {
+        public void sumPrimeNumbers(long toWhatNumber)
+        {
+            long total = 28;
+            long i;
+            bool flag = false;
+            for (i = 13; i < toWhatNumber + 1; i++)
+            {
+                int upperBound = (int)Math.Sqrt(i) + 1;
+                flag = false;
+                for (long j = 2; j < upperBound; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = true;
+                        j = i;
+                    }
+                }
+                
+                if (!flag)
+                {
+                   
+                    total += i;
+                    
+
+                }
+                if (i == toWhatNumber)
+                {
+                    Console.WriteLine(total);
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
---
+++ MathAlgorithms/PrimesUnder2Million/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimesUnder2Million
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimesUNderTwoMillionSummnation takeSum = new PrimesUNderTwoMillionSummnation();
+            takeSum.sumPrimeNumbers(2000000);
+            Console.ReadLine();
+        }
+    }
+}
