18e410a40a5ad0ab90520231c8b1c2901969bfa1
Spencer Nussrallah <spencer.nussrallah@gmail.com>
2015-10-14 00:01:34 -0500

    Do Not Look Unless You are Fully Aware that you will be revealing a solution to ProjectEuler

---
+++ MathAlgorithms/LargestPrimeFactor/LargestPrimeFactor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPrimeFactor
+{
+    class LargestPrimeFactor
+    {
+        public void Factorization(long largeNumber)
+        {
+            long upperBound = (long)Math.Sqrt(largeNumber);
+            Console.WriteLine(upperBound);
+
+            for (long i = 1;  i < upperBound; i++)
+			    {
+                Console.WriteLine(i);
+                if (largeNumber % i == 0)
+                {
+                    Console.WriteLine(i);
+                    int newUpperBound = (int)Math.Sqrt(i);
+                    bool flag = false;
+                    for (int j = 2; j < newUpperBound; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            j = newUpperBound;
+                            flag = true;
+                        }
+                    }
+                    if (!flag)
+                    {
+                        Console.WriteLine(i);
+                        Console.ReadLine();
+                    }
+                    {
+
+                    }
+                   
+                }
+			    }
+            Console.WriteLine(largeNumber);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MathAlgorithms/LargestPrimeFactor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LargestPrimeFactor lpf = new LargestPrimeFactor();
+            lpf.Factorization(600851475143);
+        }
+    }
+}
