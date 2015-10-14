8a89b827ac0143e7b5f38e92ad21e22272afb224
Spencer Nussrallah <spencer.nussrallah@gmail.com>
2015-10-14 00:02:16 -0500

    Do Not Look Unless You are Fully Aware that you will be revealing a solution to ProjectEuler

---
+++ MathAlgorithms/OnethousandthDigit/Calculate1000thTerm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneThousandDigitFibonacciNumbe
+{
+    class Calculate1000thTerm
+    {
+        double total = 0;
+        public void calculate()
+        {
+            double phi = ((1 + Math.Sqrt(5)) / 2);
+            total = Math.Ceiling(((1000 - 1 + (Math.Log10(5) / 2)) / (Math.Log10(phi))));
+            Console.WriteLine(total);
+        } 
+
+    }
+}
---
+++ MathAlgorithms/OnethousandthDigit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneThousandDigitFibonacciNumbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate1000thTerm calc = new Calculate1000thTerm();
+            calc.calculate();
+            Console.ReadLine();
+        }
+
+        private static void calculateFibonanci()
+        {
+            double hugeNumber = Math.Pow(10, 1000);
+            Console.WriteLine(hugeNumber);
+            double firstNum = 0;
+            double secondNum = 1;
+            double total = 0;
+            for (int i = 0; i < hugeNumber; i++)
+            {
+                Console.Write(i + ":");
+                if (i == 0)
+                {
+                    Console.WriteLine(firstNum);
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine(secondNum);
+                }
+                
+                else
+                {
+                    total = firstNum + secondNum;
+                    firstNum = secondNum;
+                    secondNum = total;
+                    Console.WriteLine(total);
+
+              }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
